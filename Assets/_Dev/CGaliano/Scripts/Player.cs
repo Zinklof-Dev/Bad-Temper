aa33a5b5f187606ff00ff1c8b66791e919c26494
Zink <123103261+Zinklof@users.noreply.github.com>
2024-09-10 10:57:38 -0400

    Update Player.cs

--- Assets/_Dev/CGaliano/Scripts/Player.cs
+++ Assets/_Dev/CGaliano/Scripts/Player.cs
@@ -15,39 +15,110 @@ public class Player : MonoBehaviour
     [SerializeField] private float terminalVelocity;
     [SerializeField] private Vector3 velocity;
     [SerializeField] private float drag;
+    [Header("Camera")]
+    [SerializeField] private float sensitivity;
+    [SerializeField] private Vector3 cameraRotationEuler;
     [Space(20)]
     [Header("References")]
     [SerializeField] private CharacterController characterController;
+    [SerializeField] private Camera playerCamera;
+
+    private void XRotation()
+    {
+        float x = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        cameraRotationEuler += new Vector3(-x, 0, 0);
+        if (cameraRotationEuler.x > 90)
+        {
+            cameraRotationEuler.x = 90;
+        }
+        if (cameraRotationEuler.x < -90)
+        {
+            cameraRotationEuler.x = -90;
+        }
+
+        playerCamera.transform.localRotation = Quaternion.Euler(cameraRotationEuler);
+    }
+
+    private void YRotation()
+    {
+        float y = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+        gameObject.transform.Rotate(0, y, 0);
+    }
+
+
+    private float CalculateY()
+    {
+        float y = 0;
+
+        if (characterController.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                y = jumpForce;
+            }
+            else
+            {
+                velocity.y = -0.01f;
+            }
+        }
+        else
+        {
+            y = Physics.gravity.y * gravityMult * Time.deltaTime;
+        }
+        return y;
+    }
+
+    private Vector3 CalculateXZ()
+    {
+        Vector3 xz = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            xz.z = Input.GetAxis("Vertical") * movementSpeed * sprintMult * Time.deltaTime;
+            xz.x = Input.GetAxis("Horizontal") * movementSpeed * sprintMult * Time.deltaTime;
+        }
+        else
+        {
+            xz.z = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+            xz.x = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        }
+        return xz;
+    }
+
+    private void CalculateVelocityChanges()
+    {
+        velocity.y = Mathf.Clamp(velocity.y, -terminalVelocity, terminalVelocity);
+        velocity.x = Mathf.Clamp(velocity.x, -terminalVelocity, terminalVelocity);
+        velocity.z = Mathf.Clamp(velocity.z, -terminalVelocity, terminalVelocity);
+        velocity.x = velocity.x * (1 - Time.deltaTime * drag);
+        velocity.z = velocity.z * (1 - Time.deltaTime * drag);
+    }
 
     private void CalculateMovement()
     {
         Vector3 movement = new Vector3();
-        
-        movement.z = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-        movement.x = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
 
-        if (characterController.isGrounded)
-        {
-            movement.y = -0.01f;
-        }
-        else
-        {
-            movement.y = Physics.gravity.y * gravityMult * Time.deltaTime;
-        }
+        movement = CalculateXZ();
+        movement.y = CalculateY();
 
         velocity += movement;
 
-        velocity.y = Mathf.Clamp(velocity.y, -terminalVelocity, terminalVelocity);
-        velocity.x = Mathf.Clamp(velocity.x, -terminalVelocity, terminalVelocity);
-        velocity.z = Mathf.Clamp(velocity.z, -terminalVelocity, terminalVelocity);
-        velocity = velocity * (1 - Time.deltaTime * drag);
+        CalculateVelocityChanges();
 
         characterController.Move(velocity);
     }
 
     private void Update()
     {
-        CalculateMovement();
+        try
+        {
+            XRotation();
+            YRotation();
+            Cursor.lockState = CursorLockMode.Confined;
 
+            CalculateMovement();
+        }
     }
 }
