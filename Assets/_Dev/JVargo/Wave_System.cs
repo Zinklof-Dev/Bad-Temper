95c1e436500d0067a80a46db031b271b46509709
DJLuigi00 <102384339+DJLuigi00@users.noreply.github.com>
2024-09-11 14:05:41 -0400

    9/11/24-1-LJV

---
+++ Assets/_Dev/JVargo/Wave_System.cs
@@ -0,0 +1,27 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+using ZinklofDev.Console;
+
+public class Wave_System : NetworkBehaviour
+{
+    public NetworkVariable<Int32> waveCount = new NetworkVariable<Int32>(
+        value: 0,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Server
+        );
+
+    public override void OnNetworkSpawn()
+    {
+
+
+
+        base.OnNetworkSpawn();
+    }
+
+    public static Command
+    void Update()
+    {
+        
+    }
+}
