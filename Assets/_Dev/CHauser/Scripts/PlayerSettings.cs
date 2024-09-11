1fb9e57821529d530fed5b2a52de0e2615795798
HauserCGearSpin <158326781+HauserCGearSpin@users.noreply.github.com>
2024-09-11 14:05:17 -0400

    9/11 Work
    Made a static player setting class with some basic variables that we can add on to later. Also started castle script.

---
+++ Assets/_Dev/CHauser/Scripts/PlayerSettings.cs
@@ -0,0 +1,11 @@
+using Unity.Collections;
+using UnityEngine;
+
+public static class PlayerSettings
+{
+    public static FixedString32Bytes userName;
+    public static bool fullScreen;
+    public static bool volumeMuted;
+    public static float sensitivity;
+    public static float volume;
+}
