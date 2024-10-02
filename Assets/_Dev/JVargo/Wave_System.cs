f508537aaaafa21f04874068b17e64184120aca0
DJLuigi00 <102384339+DJLuigi00@users.noreply.github.com>
2024-10-02 14:06:57 -0400

    10/2/24 JV 2:06

--- Assets/_Dev/JVargo/Wave_System.cs
+++ Assets/_Dev/JVargo/Wave_System.cs
@@ -2,6 +2,7 @@ using System;
 using Unity.Netcode;
 using UnityEngine;
 using ZinklofDev.Console;
+using ZinklofDev.Utils.Testing;
 
 public class Wave_System : NetworkBehaviour
 {
@@ -11,8 +12,31 @@ public class Wave_System : NetworkBehaviour
         NetworkVariableWritePermission.Server
         );
 
+    public void increaseWaveCount(int incremental)
+    {
+        if (!IsOwner) 
+            return;
+        if (!IsServer)
+            return;
+
+        waveCount.Value += incremental;
+        Debug.Log(waveCount.Value);
+    }
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
     }
+    public void Update()
+    {
+        Debug.Log(waveCount);
+    }
+
+    public static Test WaveManagerIncramentTest = new Test("Wave_System.cs", () =>
+    {
+      //   int wavecount = increaseWaveCount("");
+
+       // WaveManagerIncramentTest.Expect();
+    });
+    
 }
