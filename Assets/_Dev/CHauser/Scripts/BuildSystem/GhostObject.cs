0eb1b6917156a2471dd58612cbad442a51bdfe2e
HauserCGearSpin <158326781+HauserCGearSpin@users.noreply.github.com>
2024-10-18 14:18:18 -0400

    Update GhostObject.cs
    More scrapping. Also, just making new system to see if it works, I have no idea if it will or how it will impact performance. Will talk to Cameron about it.

--- Assets/_Dev/CHauser/Scripts/BuildSystem/GhostObject.cs
+++ Assets/_Dev/CHauser/Scripts/BuildSystem/GhostObject.cs
@@ -2,35 +2,12 @@ using UnityEngine;
 
 public class GhostObject : MonoBehaviour
 {
-    public bool isSpawnable;
-    public float raduisOfCheck;
+    public bool isSpawnable; 
     public Vector3 defaultPosition;
-    bool done = false;
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(transform.position, raduisOfCheck);
-    }
 
     private void Update()
     {
         if (transform.position == defaultPosition)
             isSpawnable = false;
-
-        Collider[] thingsInBounds = Physics.OverlapSphere(transform.position, raduisOfCheck); 
-
-        foreach(Collider collider in thingsInBounds)
-        {
-            if(!done)
-            {
-                isSpawnable = true;
-            }
-
-            if(collider.gameObject.tag == "BuildingObjects")
-            {
-                isSpawnable = false;
-                done = true;
-            }
-        }
     }
 }
