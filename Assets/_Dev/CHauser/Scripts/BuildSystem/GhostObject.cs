5b9ac5495a0f16b2a7a0cc39e30b526fe8b7b5a9
HauserCGearSpin <158326781+HauserCGearSpin@users.noreply.github.com>
2024-10-21 17:31:16 -0400

    Update GhostObject.cs
    Added an overlap box function to supplement the checking system, I hope this works :) (Please work)

--- Assets/_Dev/CHauser/Scripts/BuildSystem/GhostObject.cs
+++ Assets/_Dev/CHauser/Scripts/BuildSystem/GhostObject.cs
@@ -4,12 +4,27 @@ public class GhostObject : MonoBehaviour
 {
     public bool isSpawnable; 
     public Vector3 defaultPosition;
+    public LayerMask layerMask;
 
     private void Update()
     {
+        if (CheckForCollision())
+            isSpawnable = true;
+        else
+            isSpawnable = false;
+
         if (transform.position == defaultPosition)
             isSpawnable = false;
         else
             isSpawnable = true;
+    }
+
+    private bool CheckForCollision()
+    {
+        Collider[] colliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.Identity, layerMask);
+        if (colliders[0] == null)
+            return true;
+        else
+            return false; 
     }
 }
