86cf0605933fe3dd5c1e888507e4f647005476a3
Zink <123103261+Zinklof@users.noreply.github.com>
2024-10-29 08:25:08 -0400

    aicraetor
    added camera looksie code :de:

--- Assets/_Dev/CGaliano/Scripts/AICreator/ThemeCreatorObject.cs
+++ Assets/_Dev/CGaliano/Scripts/AICreator/ThemeCreatorObject.cs
@@ -8,6 +8,7 @@ namespace BadTemper.AICreator
     public class ThemeCreatorObject : MonoBehaviour
     {
         [SerializeField] List<Theme> themeList = new List<Theme>();
+        [SerializeField] byte currentTheme;
 
         private void OnValidate()
         {
@@ -16,18 +17,21 @@ namespace BadTemper.AICreator
                 Debug.LogError("Theme system only supports 255 themes, please remove one to avoid errors!");
             }
 
-            if (!CurrentTheme.themeHasBeenSet)
-            {
-                ChangeCurrentTheme(0);
-            }
+            //if (!CurrentTheme.themeHasBeenSet)
+            //{
+            //    ChangeCurrentTheme(0);
+            //}
+
+            ChangeCurrentTheme(currentTheme);
         }
 
         private void Start()
         {
-            if (!CurrentTheme.themeHasBeenSet)
-            {
-                ChangeCurrentTheme(0);
-            }
+            //if (!CurrentTheme.themeHasBeenSet)
+            //{
+            //    ChangeCurrentTheme(0);
+            //}
+            ChangeCurrentTheme(currentTheme);
         }
 
         public void ChangeCurrentTheme(byte themeID)
