9cce86195a48461c28973b1a1c328069e2bf7b7b
HauserCGearSpin <158326781+HauserCGearSpin@users.noreply.github.com>
2024-10-29 16:37:34 -0400

    Update FloorObject.cs
    - not +

--- Assets/_Dev/CHauser/Scripts/BuildSystem/FloorObject.cs
+++ Assets/_Dev/CHauser/Scripts/BuildSystem/FloorObject.cs
@@ -10,6 +10,6 @@ public class FloorObject : MonoBehaviour
         wallSpots[0] = new Vector3(transform.position.x + offset, transform.position.y, transform.position.z);
         wallSpots[1] = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z);
         wallSpots[2] = new Vector3(transform.position.x, transform.position.y, transform.position.z + offset);
-        wallSpots[3] = new Vector3(transform.position.x, transform.position.y, transform.position.z + offset);
+        wallSpots[3] = new Vector3(transform.position.x, transform.position.y, transform.position.z - offset);
     }
 }
