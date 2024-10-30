bc9d2148de82eb2551518f6ed04ef5f525aaeb3f
Zink <zinklofbusiness@gmail.com>
2024-10-30 08:24:57 -0400

    Update FloorObject.cs
    this may or may not have compiler errors, did it on web sorry.

--- Assets/_Dev/CHauser/Scripts/BuildSystem/FloorObject.cs
+++ Assets/_Dev/CHauser/Scripts/BuildSystem/FloorObject.cs
@@ -1,15 +1,46 @@
+using System;
 using UnityEngine;
+
+[Serializable]
+public struct WallPoint
+{
+    public Vector3 pos;
+    public Vector3 eulerRotation;
+    public Quaternion quaternionRotation { get; private set; }
+
+    public WallPoint(Vector3 pos, Vector3 eulerRotation)
+    {
+        this.pos = pos;
+        this.eulerRotation = eulerRotation;
+
+        quaternionRotation = Quaternion.Euler(eulerRotation);
+    }
+}
 
 public class FloorObject : MonoBehaviour
 {
-    [SerializeField] private Vector3[] wallSpots;
-    private float offset = 0.5f;
+    [SerializeField] List<WallPoint> inpsectorWallPoints = new List<WallPoint>;
 
-    private void Start()
+    private void OnDrawGizmos()
     {
-        wallSpots[0] = new Vector3(transform.position.x + offset, transform.position.y, transform.position.z);
-        wallSpots[1] = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z);
-        wallSpots[2] = new Vector3(transform.position.x, transform.position.y, transform.position.z + offset);
-        wallSpots[3] = new Vector3(transform.position.x, transform.position.y, transform.position.z - offset);
+        foreach(WallPoint point in inspectorWallPoints)
+        {
+            
+            Gizmos.color = Color.Blue;
+            Gizmos.DrawSphere(point.pos, 0.25f);
+            //Gizmos.DrawLine(point.pos, )
+            //this is commented because i ran out of time, you'll need to use the euler rotation to find another point infront of the wallpoint in the direction your quaternion rotation points
+            //Then you'll have a debug view of the points and where they are facing which is mostly for QOL and later usage.
+        }
+    }
+
+    public List<WallPoint> GetWallPoints();
+    {
+        List<WallPoint> tempWallPointList = new List<WallPoints>;
+        
+        //this is simple logic, you can tell why we use this func
+        
+        return tempWallPointList;
     }
 }
+    
