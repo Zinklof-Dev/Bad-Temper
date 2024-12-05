2cfe13c12e77e2c6e64a8de69f4afdc741697cdb
Zink <zinklofbusiness@gmail.com>
2024-12-05 08:24:09 -0500

    Update PlayersManager.cs
    should work? expect comp errors, expect minor fixes needed later.

--- Assets/!Game/Scripts/Mainmenu/Multiplayer/PlayersManager.cs
+++ Assets/!Game/Scripts/Mainmenu/Multiplayer/PlayersManager.cs
@@ -7,6 +7,7 @@ public class PlayersManager : NetworkBehaviour
 {
     [SerializeField] ulong[] slots = new ulong[6];
     [SerializeField] FixedString32Bytes[] usernames = new FixedString32Bytes[6];
+    [SerializeField} GameObject[] playerObjects = new GameObject[6];
     [SerializeField] ulong clientId;
 
     private void Start()
@@ -26,11 +27,48 @@ public class PlayersManager : NetworkBehaviour
         if (IsOwner)
         {
             AskForIdRpc();
+            GiveServerUsernameRpc((FixedString32Bytes)ClientBackend.playerUsername) 
+        }
+    }
+
+    void ReconfigurePlayerScreen() // run locally on each client after they receive a new name. this will update all names on hud that are active, and activate any that dont have the keyword for no player. this isn't the best solution but its a solution for now. the biggest issue is the no player keyword that a player connecting with their name being that wont be shown on the main menu.
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            GameObject go = playerObjects[i]; // fetch object from the array
+
+            if (go == null) // null case, shouldn't happen but you never know
+            {
+                go = GameObject.Find("Player" + i); // find the object
+                
+                if (go == null) // if that failed skip this itteration of the loop
+                {
+                    debug.logError("couldn't find player HUD object, error not fatal, user experience may be harmed though");
+                    continue;
+                }
+
+                else // otherwise save the object for later use
+                playerObjects[i] = go;
+            }
+
+            if (go.activeSelff == false && usernames[i] != "NoPlAyEr") // if the object is inactive but the slot doesn't have the no player keyword, then enable it
+            go.SetActive(true);
+            else // otherwise set it false (to be sure)
+            {
+                go.SetActive(false); //redundant call but just making absolute sure.
+                continue;
+            }
+            
+            TextMeshProUGUI tmp = go.GetComponent<TextMeshProUGUI>(); // fetch the tmp component
+
+            // add null case soon
+            
+            tmp.text = (string)usernames[i]; // change tmp text to username text
         }
     }
 
     [Rpc(SendTo.Server)]
-    void AskForIdRpc(ServerRpcParams rpcParams = default)
+    void AskForIdRpc(RpcParams rpcParams = default)
     {
         clientId = rpcParams.Receive.SenderClientId;
 
@@ -46,7 +84,7 @@ public class PlayersManager : NetworkBehaviour
     }
 
     [Rpc(SendTo.Server)]
-    void GiveServerUsernameRpc(FixedString32Bytes username, RpcParams rpcParams)
+    void GiveServerUsernameRpc(FixedString32Bytes username, RpcParams rpcParams = default)
     {
         clientId = rpcParams.Receive.SenderClientId;
         bool nameSaved = false;
@@ -59,23 +97,25 @@ public class PlayersManager : NetworkBehaviour
                 usernames[i] = username;
                 nameSaved = true;
             }
+        }
 
-            if (nameSaved == false)
-            {
-                Debug.LogError("failed to save username, canceling RPC");
-                return;
-            }
-            else
-            {
-
-            }
+        if (nameSaved == false)
+        {
+            Debug.LogError("failed to save username, canceling RPC");
+            return;
+        }
+        else
+        {
+            SendUsernameAcrossNetworkRpc(usernames[i], i);
+            return;
         }
     }
 
     [Rpc(SendTo.ClientsAndHost)]
-    void SendUsernameAcrossNetworkRpc(FixedString32Bytes username, int index)
+    void SendUsernameAcrossNetworkRpc(FixedString32Bytes username, int index, RpcParams rpcParams = default)
     {
-        //need logic
+        usernames[index] = username;
+        ReconfigurePlayerScreen();
     }
 
     [Rpc(SendTo.SpecifiedInParams)]
