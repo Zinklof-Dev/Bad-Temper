9c10ed8b72989da8dfbe2ab4d5aac68071dd0b76
Zink <zinklofbusiness@gmail.com>
2024-12-06 09:15:45 -0500

    Create TreeGenerationEditor.cs
    comp errors expected may not work, not tested

---
+++ Assets/Editor/TreeGenerationEditor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeOf(TreeGeneration))]
+Public Class TreeGenerationEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        TreeGeneration tg = (TreeGeneration)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (tg.autoUpdateInEditor)
+            {
+                tg.DrawPerlinEditor();
+            }
+            if (tg.autoUpdateTreeVisibility)
+            {
+                tg.TreeHiderEditor();
+            }
+        }
+
+        if(GUILayout.Button("Generate Perlin"))
+        {
+            tg.DrawPerlinEditor();
+        }
+        if(GUILayout.Button("Generate Trees"))
+        {
+            tg.GenTreesEditor();
+        }
+        if(GUILayout.Button("Re Compute Hidden Trees")
+        {
+            tg.TreeHiderEditor();
+        }
+        if(GUILayout.Button("Clear Editor Memory"))
+        {
+            tg.ClearEditorOnlyVariables();
+        }
+    }
+}
