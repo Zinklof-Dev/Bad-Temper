0040290da479a5e3ad1ab85fce3366b14ec0edf8
Zink <zinklofbusiness@gmail.com>
2024-12-06 13:00:09 -0500

    Create ProfileMenu.cs

---
+++ Assets/!Game/Scripts/Mainmenu/Profile/ProfileMenu.cs
@@ -0,0 +1,26 @@
+using System;
+using Unity;
+using TMPro;
+
+public class ProfileMenu : MonoBehaviour
+{
+  [SerializeField] TextMeshProUGUI username;
+
+  private Profile profile;
+
+  public void UpdateUI()
+  {
+    profile = ProfileSystem.FetchProfile();
+
+    username.text = profile.username.ToString();
+  }
+
+  public void SubmitChanges()
+  {
+    profile = ProfileSystem.FetchProfile();
+    
+    profile.username = username.text;
+
+    ProfileSystem.SaveProfileChanges(profile);
+  }
+}
