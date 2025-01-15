ded6479c06e4ad37fa60abef7c184f0ec8fd67a9
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2025-01-15 14:05:57 -0500

    1/15/25 Work - 2
    Created start of syncing tree breaking over network.

--- Assets/_Dev/KSanPedro/Scripts/TreeGeneration.cs
+++ Assets/_Dev/KSanPedro/Scripts/TreeGeneration.cs
@@ -205,9 +205,9 @@ public class TreeGeneration : NetworkBehaviour
                     int randomRotation = randomRotationValue.Next(0, 360);
                     Vector3 eulerRandomRotation = new Vector3(0, randomRotation, 0);
                     Quaternion quaternionRandomRotation = Quaternion.Euler(eulerRandomRotation);
-                    GameObject refrence = Instantiate(_TreePrefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), quaternionRandomRotation);
-                    Tree tree = new Tree(refrence, treeIndex);
-                    _NetworkTreeManager.trees.Add(tree);
+                    //Instantiate(_TreePrefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), quaternionRandomRotation);
+                    GameObject tree = GameObject.Instantiate(_TreePrefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), quaternionRandomRotation) as GameObject;
+                    _NetworkTreeManager.trees.Add(new Tree(tree, treeIndex));
                     treeIndex++;
                     //_TreeManager.AddTree(Matrix4x4.TRS(new Vector3(hit.point.x, hit.point.y, hit.point.z), quaternionRandomRotation, Vector3.one), 0);
                 }
