7939e6e22195779ade434e967bfde666549bcfa7
Zink <zinklofbusiness@gmail.com>
2025-01-16 07:53:39 -0500

    Create PlayerV2.cs
    creating own basic physics implementation because unity is mean.

---
+++ Assets/_Dev/CGaliano/Scripts/PlayerV2.cs
@@ -0,0 +1,21 @@
+/*using System;
+using UnityEngine;
+using Unity.Collections;
+using Unity.Netcode;
+using ZinklofDev.ConsoleV2;
+using TMPro;
+
+public class PlayerV2 : NetworkBehavior
+{
+    [Header("References")
+    [SerializeField] GameObject _playerCamera;
+    [SerializeField] TextMeshPro _usernameObject;
+    [space(10)]
+    [Header("Movement Physics")]
+    [SerializeField] float movementSpeed;
+    [SerializeField] float sprintMult;
+    [SerializeField] float jumpForce;
+    [SerializeField] float gravityMult;
+    [SerializeField] float terminalVelo;
+    [SerializeField] float drag;
+}*/
