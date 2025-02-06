decf72f25a33291f0faa0c34cd6471476dc9e77e
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2025-02-06 10:27:23 -0500

    Update BuildObject.cs
    Comp error fixed and fixed a bug with the prevServerHealth.

--- Assets/_Dev/CHauser/Scripts/BuildSystem/BuildObject.cs
+++ Assets/_Dev/CHauser/Scripts/BuildSystem/BuildObject.cs
@@ -8,7 +8,7 @@ public class BuildObject : NetworkBehaviour
 /*
     public float serverHealth = 100; // Health that is stored only on the server side. This is the main health that will be used in calculating when to break and other calculations based on health.
     public float localHealth = 100; // This health is stored on the client, and is the health that the client updates and reads. Can be used for health bar system or for potential healing that the client does
-    public float prevServerHealth; // This value will save the server health at the end of ServerUpdate so we only update the local health when the server health changes
+    public float prevServerHealth = 100; // This value will save the server health at the end of ServerUpdate so we only update the local health when the server health changes
     public bool hasCriticalObject = false;
     public GameObject criticalObject;
 
@@ -16,13 +16,14 @@ public class BuildObject : NetworkBehaviour
     {
         Server server = GameObject.FindGameObjectWithTag("Server").GetComponent<Server>();
         server.ServerTick += ServerUpdate;
-
+        prevServerHealth = serverHealth;
+        
         base.OnNetworkSpawn();
     }
 
     private void ServerUpdate()
     {
-        if(prevServerHealth != serverHealth
+        if(prevServerHealth != serverHealth)
         {
             UpdateLocalHealthRPC(serverHealth);
             prevServerHealth = serverHealth;
