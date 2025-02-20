e36cb79ac8eacb76f238c5590e4d69d542c29330
Zink <zinklofbusiness@gmail.com>
2025-02-20 13:11:57 -0500

    Update Player.cs
    more work:
    fake "IK" for the neck + spine when looking up and down. 
    some water physics? i think i need more? i got distracted 🤷 
    Plan for leg IK commented in, still need to actually setup IK in maya for the players skeleton.

--- Assets/_Dev/CGaliano/Scripts/Player/Player.cs
+++ Assets/_Dev/CGaliano/Scripts/Player/Player.cs
@@ -1,3 +1,17 @@
+/*
+Abreviations commonly found throughout my work, and their meaning:
+
+rot = rotation
+mem = memory
+tpv = third person view 
+tpp = third person perspective (used less often)
+fpv = first person view
+fpp = first person perspective (used less often)
+IK = inverse kinematics
+cmd = command
+
+other uncommon abreviations will have their meaning in a comment next to them or on a neighboring line.
+*/
 /*using Unity;
 using System;
 
@@ -8,6 +22,7 @@ Namespace BadTemper
     [Header("References")]
     [SerializeField] CharacterControler cc;
     [SerializeField] GameObject cameraObject;
+    [SerializeField] transform[] jointReferences; // 0 should be the head / skull joint | 1 should be neck | 2 should be top spine joint | 3 should be mid spine joint
     [Header("Movement Varaibles")]
     [SerializeField] float moveSpeed;
     [SerializeField] float sprintMult;
@@ -19,6 +34,7 @@ Namespace BadTemper
     [SerializeField] float waterDrag;
     [SerializeField] float friction;
     [SerializeField] float terminalVelocity;
+    [SerializeField] float waterLevel; // universal since the game only has an ocean and no lakes (as of now) so we don't need any complex system to check for water (would probably just set up a Volume system for that if ever needed)
     [Header("Perspective")]
     [SerializeField] Vector3 TPVCamOffset
     [SerializeField] float TPVCamLerpT;
@@ -36,13 +52,19 @@ Namespace BadTemper
     private void Start() // change to on network spawn later
     {
       playerRef = this;
+
+      cameraObject = GameObject.FindByTag("Main Camera");
+      cameraObject.transform.SetParent(jointReferences[0])
     }
     
     private void CameraHandeler()
     {
+      playerLookXY.y += Input.GetAxis("MouseY") * sensitivity * Time.deltaTime;
+      playerLookXY.x = Mathf.Clamp(PlayerLookXY.x + (Input.GetAxis("MouseX") * sensitivity * Time.deltaTime), -90, 90);
+    
       if (!TPV)
       {
-        firstPerson();
+        FPB();
       }
       else // this is not the most efficient method but TPV will likely only be a debug tool. if ever implimented properly will make more efficent code.
       {
@@ -57,13 +79,55 @@ Namespace BadTemper
       }
     }
 
-    private void firstPerson()
+    private void FirstPerson()
     {
-      playerLookXY.y += Input.GetAxis("MouseY") * sensitivity * Time.deltaTime;
-      playerLookXY.x = Mathf.Clamp(PlayerLookXY.x + (Input.GetAxis("MouseX") * sensitivity * Time.deltaTime), -90, 90);
+      // OLD SYSTEM
+      // playerLookXY.y += Input.GetAxis("MouseY") * sensitivity * Time.deltaTime; // this and the following line were moved to the CameraHandeler function to make the inputs more universal
+      // playerLookXY.x = Mathf.Clamp(PlayerLookXY.x + (Input.GetAxis("MouseX") * sensitivity * Time.deltaTime), -90, 90);
 
       transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, playerLookXY.y, transform.rotation.z));
       cameraObject.transform.localRotation = Quaternion.Euler(new Vector3(playerLookXY.x, cameraObject.transform.localRotation.y, cameraObject.transform.localRotation.z));
+    }
+
+    private void FPB() // stands for "First Person Bones" redesign of the look system to dynamically use the joints of the player to make their movement more fluid-esc | Could have used IK for this tbh... meh... actually isn't this just really basic IK?
+    {
+      // this system will use the WANTED X value in playerLookXY to rotate the spine and neck joints in order to look ~90 down or ~90 up.
+
+      // Plan: when looking 90 degrees down, neck should account for ~95% of rotation, when looking 90 degrees up, neck should account for ~75% (human neck can bend further forward than it can backwards)
+      float x = playerLookXY.x
+      
+      if (x < 0)
+      {
+        float neckRot = x * 0.95f;
+        float spineTopRot = x * 0.025f;
+        float SpineMidRot = x * 0.025f;
+      }
+      else if (x > 0)
+      {
+        float neckRot = x * 0.75f;
+        float spineTopRot = x * 0.10f;
+        float SpineMidRot = x * 0.15f;
+      }
+      else
+      {
+        float neckRot = 0;
+        float spineTopRot = 0;
+        float SpineMidRot = 0;
+      }
+
+      jointReferences[1].localRotation = Quaternion.Euler(new Vector3(neckRot, jointReferences[1].localRotation.y, jointReferences[1].localRotation.z));
+      jointReferences[2].localRotation = Quaternion.Euler(new Vector3(spineTopRot, jointReferences[2].localRotation.y, jointReferences[2].localRotation.z));
+      jointReferences[3].localRotation = Quaternion.Euler(new Vector3(spineMidRot, jointReferences[3].localRotation.y, jointReferences[3].localRotation.z));
+    }
+
+    private void IKHandeler() // NOTE 2/20/2025 CURRENT MODEL DOES NOT HAVE ANY IK JOINTS! WILL ADD TO THE MODEL SOON!
+    {
+      // neat little resource, explains about what I thought i'd have to do going into this, but also clarifies what I wasn't quite sure on: https://weaverdev.io/projects/bonehead-procedural-animation/
+    
+      // only planning to do IK for legs, meaning the only joints not controlled by code are the arm hierarchies. which will allow for more fluid animation for attacks and such rather than wonky procedural IK.
+
+      // Once IK is set up on the model this should be as simple as moving a foot joints world POS to the direction of the velocity when it gets too far away, and ensuring to alternate between legs.
+      // could go a little more complex and have a system to ensure that the foot joints don't enter a "bad area" (like the left foot going to the right side of the character)
     }
 
     private void MovementHandeler()
@@ -79,19 +143,21 @@ Namespace BadTemper
           lastJumpInput = -1;
       }
     
-      float pms = 0;
+      float mm = 0; // stands for "movement multiplier"
     
       if (Input.GetKey(KeyCode.LeftShift)
-        pms = moveSpeed * SprintMult * Time.deltaTime;
+        mm = moveSpeed * SprintMult * Time.deltaTime;
       else
-        pms = moveSpeed * Time.deltaTime;
+        mm = moveSpeed * Time.deltaTime;
 
       if (!cc.isGrounded)
-        pms *= inAirMult;
+        mm *= inAirMult;
 
       Vector3 addedMovement = ((Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward)) * pms;
 
-      if (cc.isGrounded && lastJumpInput != -1)
+      if (transform.position.y <= waterHeight && lastJumpInput != -1)
+        addedMovement.y = jumpForce; // water drag will make this cause less force than a reg jump later
+      else if (cc.isGrounded && lastJumpInput != -1)
         addedMovement.y = jumpForce;
       else if (cc.isGrounded)
         addedMovement.y = -0.01f;
@@ -101,7 +167,7 @@ Namespace BadTemper
       linearVelocity += addedMovement;
     }
 
-    public void AFT() // Air Resistance - Friction - Terminal Velocity
+    public void WAFT() // Water Drag - Air Resistance - Friction - Terminal Velocity
     {
       // Terminal Velocity
       linearVelocity.x = Mathf.Clamp(linearVelocity.x, -terminalVelocity, terminalVelocity);
@@ -111,17 +177,25 @@ Namespace BadTemper
       // Air Resistance (Drag)
       linearVelocity = linearVelocity - ((linearVelocity * drag) * Time.feltaTime);
 
-      // Friction (Drag again, but only if grounded)
+      // Friction (Drag again, but only if grounded, and not affecting Y aka jump velocity)
       if (cc.isGrounded)
-        linearVelocity = linearVelocity - ((linearVelocity * friction) * Time.deltaTime);
+      {
+        linearVelocity.x = linearVelocity.x - ((linearVelocity.x * friction) * Time.deltaTime);
+        linearVelocity.z = linearVelocity.z - ((linearVelocity.z * friction) * Time.deltaTime);
+      }
+
+      // Water Drag (Drag again, but only if under water)
+      if (transform.position.y <= waterHeight)
+        linearVelocity = linearVelocity - ((linearVelocity * waterDrag) * Time.deltaTime);
     }
 
     private void Update()
     {
       CameraHandeler();
       MovementHandeler();
-      AFT();
+      WAFT();
       cc.Move(linearVelocity);
+      IK();
     }
 
     //////// COMMANDS ////////
