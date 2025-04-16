890ae4d24239b9b9e1c13279918b72440a627b3a
Zink <zinklofbusiness@gmail.com>
2025-04-16 13:12:22 -0400

    Casting work, expect comp errors, sorry.

---
+++ Assets/!Game/Scripts/CampfireDEMO/CampfireHealing.cs
@@ -0,0 +1,43 @@
+using Unity;
+using UnityEngine;
+using System;
+
+public class campfire : MonoBehaviour
+{
+    [SerializeField] float healTimer = 4;
+    [SerializeField] float healAmount = 20;
+    [SerializeField] float radius = 3;
+    [SerializeField] bool debug;
+
+    float healCooldown;
+
+    void OnDrawGizmos()
+    {
+        if (!debug)
+        {
+            return;
+        }
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public void Update()
+    {
+        healCooldown += Time.deltaTime;
+
+        healCooldown = 0;
+
+        if (healCooldown >= healTimer)
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+            foreach(Collider c in colliders)
+            {
+                if (collider.gameObject.tag = "Player")
+                    collider.gameObject.GetComponent<Stats>().Damage(-healAmount);
+                else
+                    continue;
+            }
+        }
+    }
+}
